f58b78cb3606c80ad1284025c9679762b8e93e63
Alex Merced <alex@alexmerced.dev>
2022-04-10 12:24:37 -0400

    mongo connection configured

--- server.js
+++ server.js
@@ -13,13 +13,35 @@ import cors from "cors"
 import morgan from "morgan"
 import MainController from "./controllers/MainController.js"
 import APIController from "./controllers/APIController.js"
+import mongoose from "mongoose"
 
 // *********************************
 // Global Variables & Controller Instantiation
 // *********************************
 const PORT = process.env.PORT || 3333
+const MONGO_URI = process.env.MONGO_URI
 const mainController = new MainController()
 const apiController = new APIController()
+
+// *********************************
+// MongoDB Connection
+// *********************************
+mongoose.connect(MONGO_URI)
+
+mongoose.connection
+.on("open", () => console.log("Connected to Mongo"))
+.on("close", () => console.log("Disonnected from Mongo"))
+.on("error", (error) => console.log(error))
+
+// *********************************
+// Todo Model Object
+// *********************************
+const TodoSchema = new mongoose.Schema({
+    message: String,
+    completed: Boolean
+})
+
+const Todo = mongoose.model("Todo", TodoSchema)
 
 // *********************************
 // Creating Application Object
@@ -41,6 +63,12 @@ app.use(express.json())
 app.use(methodOverride("_method"))
 app.use("/static", express.static("static"))
 app.use(morgan("tiny"))
+app.use((req, res, next) => {
+    req.models = {
+        Todo
+    }
+    next()
+})
 app.use("/", MainRoutes)
 app.use("/api", APIRoutes)
 // Router Specific Middleware
